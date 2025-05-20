086d3ceeae311c9ad27781010168459370c4423a
Saurabh <saurabhjadhav8099@gmail.com>
2025-05-20 18:28:35 +0530

    Initial Commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen'>
+     {/* Theme Toggle */}
+
+     {/* Background Effects */}
+
+     {/* NavBar */}
+
+     {/* Main Content */}
+
+     {/* Footer */}
+
+    </div>
+  )
+}
+
+export default Home

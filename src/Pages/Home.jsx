274fce5ccaf7943b0022594533f19d4398a3abb4
Saurabh <saurabhjadhav8099@gmail.com>
2025-05-21 17:46:10 +0530

    BackGround and theme

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+import { StarBackground } from '../components/StarBackground'
 
 const Home = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen bg-background text-foreground overflow-hidden'>
      {/* Theme Toggle */}
-
+     <ThemeToggle/>
      {/* Background Effects */}
-
+    <StarBackground/>
      {/* NavBar */}
 
      {/* Main Content */}
